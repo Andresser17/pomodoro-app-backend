5d6899e2083c7b0b9a803ccd267ca0afa239aaa7
Andresser17 <tensasangetsu1254@gmail.com>
2022-02-12 21:20:51 -0400

    Allowed cors to localhost:3000.

--- src/index.js
+++ src/index.js
@@ -15,25 +15,10 @@ dotenv.config();
 const app = express();
 
 let corsOptions = {
-  origin: "http://localhost:3600",
+  origin: "http://localhost:3000",
 };
 
 app.use(cors(corsOptions));
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-//   res.header("Access-Control-Expose-Headers", "Content-Length");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Accept, Authorization, Content-Type, X-Requested-With, Range"
-//   );
-//   if (req.method === "OPTIONS") {
-//     return res.sendStatus(200);
-//   }
-//   return next();
-// });
 
 // Parse request of content-type - application/json
 app.use(express.json());
