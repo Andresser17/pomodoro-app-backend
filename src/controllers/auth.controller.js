ce17b5545fcc219655eef5cfdc07777c4abcf8ca
Andresser17 <tensasangetsu1254@gmail.com>
2022-02-27 16:02:41 -0400

    Refactored functions in auth.controller.js, added handleAsyncError function.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,72 +1,76 @@
-import authConfig from "../config/auth.config.js";
-import userConfig from "../config/user.config.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+// Configs
+import authConfig from "../config/auth.config.js";
+import userConfig from "../config/user.config.js";
 // Models
 import db from "../models/index.js";
 import { getRolesByName, getOneRole } from "../models/role.model.js";
+// Helpers
+import handleAsyncError from "../helpers/handleAsyncError.js";
 const { user: User } = db;
 const { refreshToken: RefreshToken } = db;
 
 export const signUp = async (req, res) => {
-  const user = await User.createUser({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8),
-    // Add default settings to new user.
-    settings: userConfig,
-  });
+  const [user, err] = await handleAsyncError(
+    User.createUser({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+      // Add default settings to new user.
+      settings: userConfig,
+    })
+  );
 
-  if (req.body.roles) {
-    const roles = await getRolesByName(req.body.roles).catch((err) => {
-      return res.status(500).send({ message: err });
-    });
+  if (err) return res.status(500).json({ err: "Internal server error" });
 
-    // Add roles to created user
-    user.roles = roles.map((role) => role._id);
-    const saved = await user.save().catch((err) => {
-      return res.status(500).send({ message: err });
-    });
+  try {
+    if (req.body.roles) {
+      const roles = await getRolesByName(req.body.roles);
 
-    return res.send({ message: "User was registered successfully!" });
-  }
-  // If roles wasn't provided.
-  const role = await getOneRole({ name: "user" }).catch((err) => {
-    return res.status(500).send({ message: err });
-  });
+      // Add roles to created user
+      user.roles = roles.map((role) => role._id);
+      await user.save();
 
-  // Add role to created user
-  user.roles = [role._id];
-  user.save().catch((err) => {
-    if (err) {
-      return res.status(500).send({ message: err });
+      return res
+        .status(200)
+        .json({ message: "User was registered successfully!" });
     }
-  });
+    // If roles wasn't provided.
+    const role = await getOneRole({ name: "user" });
 
-  return res.send({ message: "User was registered successfully!" });
+    // Add role to created user
+    user.roles = [role._id];
+    await user.save();
+  } catch (err) {
+    return res.status(500).json({ err: "Internal server error" });
+  }
+
+  return res.status(200).json({ message: "User was registered successfully!" });
 };
 
 export const signIn = async (req, res) => {
-  const user = await User.getUserByEmail(req.body.email, [
-    "roles",
-    "-__v",
-  ]).catch((err) => res.status(500).send({ message: err }));
+  const [user, err] = await handleAsyncError(
+    User.getUserByEmail(req.body.email, ["roles", "-__v"])
+  );
+
+  if (err) return res.status(500).json({ err: "Internal server error" });
 
   if (!user) {
-    return res.status(404).send({ message: "User Not found." });
+    return res.status(404).json({ message: "User Not found." });
   }
 
   const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
 
   if (!passwordIsValid) {
-    return res.status(401).send({
+    return res.status(401).json({
       accessToken: null,
       message: "Invalid Password!",
     });
   }
 
-  const token = jwt.sign({ id: user.id }, authConfig.secret, {
+  const accessToken = jwt.sign({ id: user.id }, authConfig.secret, {
     expiresIn: authConfig.jwtExpiration,
   });
 
@@ -78,12 +82,12 @@ export const signIn = async (req, res) => {
     authorities.push("ROLE_" + role.name.toUpperCase());
   }
 
-  res.status(200).send({
+  res.status(200).json({
     id: user._id,
     username: user.username,
     email: user.email,
     roles: authorities,
-    accessToken: token,
+    accessToken,
     refreshToken,
   });
 };
@@ -95,28 +99,31 @@ export const getNewAccessToken = async (req, res) => {
     return res.status(403).json({ message: "Refresh Token is required!" });
   }
 
-  const refreshToken = await RefreshToken
-    .getRefreshToken(requestToken)
-    .catch((err) => res.status(500).json({ message: err }));
+  // Get user refresh token
+  const [userRefreshToken, err] = await handleAsyncError(
+    RefreshToken.getRefreshToken(requestToken)
+  );
 
-  if (!refreshToken) {
+  if (err) return res.status(500).json({ err: "Internal server error" });
+
+  if (!userRefreshToken) {
     res.status(403).json({ message: "Refresh token is not in database!" });
     return;
   }
 
-  if (RefreshToken.verifyExpiration(refreshToken)) {
-    RefreshToken.findByIdAndRemove(refreshToken._id, {
-      useFindAndModify: false,
-    }).exec();
+  // If user refresh token expired, remove from db
+  if (RefreshToken.verifyExpiration(userRefreshToken)) {
+    await RefreshToken.findByIdAndRemove(userRefreshToken._id);
 
     res.status(403).json({
       message: "Refresh token was expired. Please make a new signin request",
     });
+
     return;
   }
 
   const newAccessToken = jwt.sign(
-    { id: refreshToken.user._id },
+    { id: userRefreshToken.user._id },
     authConfig.secret,
     {
       expiresIn: authConfig.jwtExpiration,
@@ -125,6 +132,6 @@ export const getNewAccessToken = async (req, res) => {
 
   return res.status(200).json({
     accessToken: newAccessToken,
-    refreshToken: refreshToken.token,
+    refreshToken: userRefreshToken.token,
   });
 };
